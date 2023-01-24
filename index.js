957291fadd4e20ea1a496f16e3bb509d34c742da
CHIRAG193 <66202052+CHIRAG193@users.noreply.github.com>
2023-01-24 15:53:33 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const app=express()
+
+const users = [
+    {
+    id: 1,
+    name: 'John',
+    email: 'john@example.com',
+    age: 30,
+    city: 'New York',
+    state: 'NY'
+    },
+    {
+    id: 2,
+    name: 'Jane',
+    email: 'jane@example.com',
+    age: 25,
+    
+    city: 'Chicago',
+    state: 'IL'
+    },
+    {
+    id: 3,
+    name: 'Bob',
+    email: 'bob@example.com',
+    age: 35,
+    city: 'New York',
+    state: 'NY'
+    }
+];
+
+getUsersInState(users, state)
+{
+    users.find((state,index)=>{
+        
+         console.log( [ users.id,users.name,users.email,users.age,users.city,users.state])
+        
+    })
+
+}
+getUsersInState(users,'NY');
+const port=process.env.port || 3000;
+app.listen(port,()=>{
+    console.log(`server is running  on${port}`)
+})
